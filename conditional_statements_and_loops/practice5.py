83f24e9c98bdaf57cd4be6e3fd48ba3bf5d0139d
sanusa644 <sanusa644@gmail.com>
2024-04-02 11:13:56 +0200

    practices

---
+++ conditional_statements_and_loops/practice5.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program that accepts a word from the user and reverses it.
+"""
+
+word = input("Enter a word: ")
+
+
+print(word[::-1])
---
+++ conditional_statements_and_loops/practice7.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
+Note : Use 'continue' statement.
+Expected Output : 0 1 2 4 5
+"""
+
+for number in range(7):
+    if number == 3 or number == 6:
+        continue
+    else:
+        print(number, end=" ")
+print("\n")
